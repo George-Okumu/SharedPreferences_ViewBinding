ed3fa3b123b0f965bb71c37d9f6385386d97d589
George Okumu <gokumu12@gmail.com>
2021-09-13 01:29:31 +0300

    Saved Data into sharedPreference

---
+++ app/src/main/java/com/moringa/sharedprefferences/Constants.java
@@ -0,0 +1,10 @@
+package com.moringa.sharedprefferences;
+
+public class Constants {
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    public static final String NAME = "name";
+    public static final String EMAIL = "email";
+    public static final String PASSWORD = "password";
+
+
+}
--- app/src/main/java/com/moringa/sharedprefferences/MainActivity.java
+++ app/src/main/java/com/moringa/sharedprefferences/MainActivity.java
@@ -2,19 +2,30 @@ package com.moringa.sharedprefferences;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
 
 import com.moringa.sharedprefferences.databinding.ActivityMainBinding;
 
+import static android.content.ContentValues.TAG;
+
 public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
 
     //For view binding
     ActivityMainBinding activityMainBinding;
-    
+
+    //sharedPreference thingy
+    private SharedPreferences sharedPreference;
+
+    //shared Preference Editor
+    private SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,13 +33,28 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         View view = activityMainBinding.getRoot();
         setContentView(view);
 
+        sharedPreference = getSharedPreferences(Constants.MyPREFERENCES, Context.MODE_PRIVATE);
+        editor = sharedPreference.edit();
+
         activityMainBinding.button1.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if(v == activityMainBinding.button1){
-            Toast.makeText(MainActivity.this, "You really did great", Toast.LENGTH_LONG).show();
-        }
+        String name = activityMainBinding.editName.getText().toString();
+        String email = activityMainBinding.editEmail.getText().toString();
+        String password = activityMainBinding.editPassword.getText().toString();
+
+
+        //Assigining the variables to their respective values inaitwa key value pair
+        editor.putString(Constants.NAME, name);
+        editor.putString(Constants.EMAIL, email);
+        editor.putString(Constants.PASSWORD, password);
+
+        editor.apply();
+
+        Toast.makeText(MainActivity.this, "Thanks, this was awesome", Toast.LENGTH_LONG).show();
+
+
     }
 }
